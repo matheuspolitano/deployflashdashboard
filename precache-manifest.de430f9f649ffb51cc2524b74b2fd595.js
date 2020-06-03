49c13a50016dd41dfa929c8db6793fbe51d77ea5
Matheus Politano <47066134+matheuspolitano@users.noreply.github.com>
2020-06-03 15:11:49 -0300

    Add files via upload

---
+++ precache-manifest.de430f9f649ffb51cc2524b74b2fd595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555820600de1ed08046c",
+    "url": "/css/app.c358a91e.css"
+  },
+  {
+    "revision": "f25fdc045d5c0d3e576c8a65f085b675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555820600de1ed08046c",
+    "url": "/js/app.9c4ff8bb.js"
+  },
+  {
+    "revision": "210b00ad8fdb0bfb5fe5",
+    "url": "/js/chunk-vendors.dd010599.js"
+  },
+  {
+    "revision": "af3442210705525bcba5cbbc7e4307bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
